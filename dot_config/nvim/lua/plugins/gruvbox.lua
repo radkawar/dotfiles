c39bfd9dd15f64cb9a552d17bb116685f619f035
radkawar <radkawar28@gmail.com>
2025-04-03 00:50:52 +0100

    fix theme

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,12 +1,29 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- Add tokyonight plugin
+  { "folke/tokyonight.nvim" },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load tokyonight colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
+  },
+
+  -- Configure tokyonight with options
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "storm", -- Options: "storm", "moon", "night", "day"
+        transparent = false, -- Set to true for transparent background
+        dim_inactive = false, -- Dim inactive windows
+        styles = {
+          comments = { italic = true },
+          keywords = { bold = true },
+        },
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
   },
 }
