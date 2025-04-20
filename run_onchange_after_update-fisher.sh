f11efb2d0d996583fc7032348afde27a5390d009
radkawar <radkawar28@gmail.com>
2025-04-20 13:29:43 +0100

    Update .config/fish/fish_plugins Update update-fisher.sh Remove update_fisher

--- run_onchange_after_update-fisher.sh
+++ run_onchange_after_update-fisher.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env fish
 
+if not functions -q fisher
+    echo "Fisher is not installed. Installing..."
+
+    echo "Installing fisher..."
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
 # fish_plugins hash: {{ include "fish/fish_plugins" | sha256sum }}
 fisher update
