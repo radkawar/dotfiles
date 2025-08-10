f830916b0c941e7b6d525c54e62d79f3b5519dc3
radkawar <radkawar28@gmail.com>
2025-08-10 12:38:31 +0100

    Update .config/nvim/lazyvim.json Add .config/nvim/lua/plugins/toggleterm.lua

---
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      -- Size can be a number or function which is passed the current terminal
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = false, -- Disable shading to avoid color issues
+      shading_factor = 0, -- No shading
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      persist_mode = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      auto_scroll = true,
+      float_opts = {
+        border = "curved",
+        width = function()
+          return math.floor(vim.o.columns * 0.8)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.8)
+        end,
+        winblend = 0, -- No transparency
+      },
+      winbar = {
+        enabled = false,
+        name_formatter = function(term)
+          return term.name
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      -- Specific keymaps for different terminal types
+      vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Float Terminal" })
+      vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Horizontal Terminal" })
+      vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Vertical Terminal" })
+
+      -- Terminal management
+      vim.keymap.set("n", "<leader>ta", "<cmd>ToggleTermToggleAll<cr>", { desc = "Toggle all terminals" })
+      vim.keymap.set("n", "<leader>ts", "<cmd>TermSelect<cr>", { desc = "Select terminal" })
+
+      -- Quick terminal switching
+      vim.keymap.set("n", "<leader>t1", "<cmd>1ToggleTerm<cr>", { desc = "Terminal 1" })
+      vim.keymap.set("n", "<leader>t2", "<cmd>2ToggleTerm<cr>", { desc = "Terminal 2" })
+      vim.keymap.set("n", "<leader>t3", "<cmd>3ToggleTerm<cr>", { desc = "Terminal 3" })
+      vim.keymap.set("n", "<leader>t4", "<cmd>4ToggleTerm<cr>", { desc = "Terminal 4" })
+
+      -- Terminal mode mappings
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts) -- Esc to exit terminal mode
+        vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts) -- jk to exit terminal mode
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      -- Apply terminal keymaps when terminal opens
+      vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+    end,
+  },
+  -- Disable Snacks terminal to avoid conflicts
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = { enabled = false },
+    },
+  },
+}
