78aa62f823aa2f2ca15e5201b1e93a341cc6578a
radkawar <radkawar28@gmail.com>
2025-08-10 12:44:51 +0100

    Update .config/nvim/lua/plugins/toggleterm.lua

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -63,7 +63,7 @@ return {
       -- Terminal mode mappings
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
-        vim.jeymap.set("t", "jj", [[<C-\><C-n>]], opts) -- jj to exit terminal mode
+        vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts) -- jj to exit terminal mode
         vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
